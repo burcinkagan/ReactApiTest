8be10dc35418a37563822a3a779a0ac00343e670
Burçin Kağan <77413560+burcinkagan@users.noreply.github.com>
2021-01-30 20:48:50 +0300

    ReactExample

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react'
+import { Card, Container } from 'semantic-ui-react';
+import { ListRelaxed } from './features/ListRelaxed';
+ 
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div>
+    <Container>
+     <ListRelaxed/> 
+    </Container>
 
-export default App;
+  </div>
+  )
+}
